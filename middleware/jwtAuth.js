cee6b8fa72e397ac0c2ca24f11a0b804a864031a
Oskar <oskarlovrencic102@gmail.com>
2022-08-28 14:59:38 -0700

    First commit

---
+++ middleware/jwtAuth.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const jwt = require('jsonwebtoken');
+
+const requireAuth = (req, res, next) => {
+
+    const token = req.cookies.jwt;
+
+    if(token){
+        jwt.verify(token, process.env.SECRET_TOKEN, (err, decodedToken) => {
+            if(err){
+                console.log(err.message);
+                res.redirect('/login');
+            }else{
+                req.user = decodedToken.id
+                next();
+            }
+        });
+        return;
+    }
+    res.redirect('/login');
+}
+
+module.exports = { requireAuth };
